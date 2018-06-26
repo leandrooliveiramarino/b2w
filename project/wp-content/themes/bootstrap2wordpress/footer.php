3f3284928b2dd36519fed454cb1c93dfeaaec1bc
Leandro Marino <leandromarino07@gmail.com>
2018-06-25 22:55:16 -0300

    Ajustar cabeçalho e rodapé

--- project/wp-content/themes/bootstrap2wordpress/footer.php
+++ project/wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,26 +11,17 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="http://leandromarino.me">Leandro Marino</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/jquery-2.1.1.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/assets/js/bootstrap.min.js"></script>
+
+<script src="<?php bloginfo('template_directory') ?>/assets/js/main.js"></script>
+
 
 </body>
 </html>
