2f2ec38951602bfc8c949f181e9bf69446bd0412
Leandro Marino <leandrooliveiramarino@gmail.com>
2018-07-11 20:19:07 -0300

    Criar página para contato e busca

--- project/wp-content/themes/bootstrap2wordpress/template-parts/content-page.php
+++ project/wp-content/themes/bootstrap2wordpress/template-parts/content-page.php
@@ -11,42 +11,40 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) :
+			?>
+			<div class="entry-meta">
+    			<div class="post-details">
+	    			<i class="fa fa-user"></i> <?php the_author(); ?>
+					<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+					<i class="fa fa-folder"></i><?php the_category(', '); ?>
+					<i class="fa fa-tags"></i> <?php the_tags(); ?></a>
+
+					<div class="post-comments-badge">
+						<a href="<?php comments_link();  ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%') ?></a>
+					</div><!-- post-comments-badge -->
+					<?php edit_post_link('Edit', '<i class="fa fa-percil"></i>', ''); ?>
+    			</div><!-- post-details -->
+			</div><!-- .entry-meta -->
+
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'bootstrap2wordpress' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
+	<?php if(has_post_thumbnail()) : ?>
+		<div class="post-image">
+			<?php the_post_thumbnail(); ?>
+		</div><!-- post-image -->
 	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+
+	<div class="post-body">
+		<?php the_content(); ?>
+	</div><!-- post-excerpt -->
+
+	<?php endif; ?>
+</article>
--- project/wp-content/themes/bootstrap2wordpress/template-parts/content-search.php
+++ project/wp-content/themes/bootstrap2wordpress/template-parts/content-search.php
@@ -11,25 +11,38 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			bootstrap2wordpress_posted_on();
-			bootstrap2wordpress_posted_by();
 			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+			<div class="entry-meta">
+    			<div class="post-details">
+	    			<i class="fa fa-user"></i> <?php the_author(); ?>
+					<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+					<i class="fa fa-folder"></i><?php the_category(', '); ?>
+					<i class="fa fa-tags"></i> <?php the_tags(); ?></a>
+
+					<div class="post-comments-badge">
+						<a href="<?php comments_link();  ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%') ?></a>
+					</div><!-- post-comments-badge -->
+					<?php edit_post_link('Edit', '<i class="fa fa-percil"></i>', ''); ?>
+    			</div><!-- post-details -->
+			</div><!-- .entry-meta -->
+
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
+	<?php if(has_post_thumbnail()) : ?>
+		<div class="post-image">
+			<?php the_post_thumbnail(); ?>
+		</div><!-- post-image -->
+	<?php endif; ?>
 
-	<div class="entry-summary">
+	<div class="post-body">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	</div><!-- post-excerpt -->
 
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
